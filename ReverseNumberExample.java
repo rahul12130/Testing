7bab60694bba0d4859239daa4d57148d55543333
rahul12130 <113431090+rahul12130@users.noreply.github.com>
2022-12-15 11:20:26 +0530

    Update and rename ReverseNumberExample1.java to ReverseNumberExample.java

--- ReverseNumberExample.java
+++ ReverseNumberExample.java
@@ -1,10 +1,8 @@
-    public class ReverseNumberExample1   
-    {  
-    public static void main(String[] args)   
-    {  
+    public class ReverseNumberExample {  
+    public static void main(String[] args) {
+        
     int number = 987654, reverse = 0;  
-    while(number != 0)   
-    {  
+    while(number != 0)   {  
     int remainder = number % 10;  
     reverse = reverse * 10 + remainder;  
     number = number/10;  
