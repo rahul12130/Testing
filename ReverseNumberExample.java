23eeadf3aee38f7b07c37bd16923efbc6cc82edb
rahul12130 <113431090+rahul12130@users.noreply.github.com>
2022-12-15 11:24:56 +0530

    Update ReverseNumberExample.java

--- ReverseNumberExample.java
+++ ReverseNumberExample.java
@@ -1,14 +1,10 @@
-    public class ReverseNumberExample
-    {  
-    public static void main(String[] args)   
-    {  
-    int number = 123456, reverse = 0;  
-    //we have not mentioned the initialization part of the for loop  
-    for( ;number != 0; number=number/10)   
-    {  
-    int remainder = number % 10;  
-    reverse = reverse * 10 + remainder;  
-    }  
-    System.out.println("The reverse of the given number is: " + reverse);  
-    }  
+ class ReverseNumberExampl {  
+    public static void main(String[] args) { 
+        int number = 123456, reverse = 0;
+        for( ;number != 0; number=number/10) { 
+            int remainder = number % 10;
+            reverse = reverse * 10 + remainder;
+        } 
+        System.out.println("The reverse of the given number is: " + reverse);
+       }  
     }  
